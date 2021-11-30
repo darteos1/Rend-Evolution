18af0d63aa7f04640207e9d46e59b940e3a14a6e
Daniel Alves <d.alves@ba.estudante.senai.br>
2021-11-30 17:41:03 -0300

    30/11

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const btnMobile=document.getElementById('btn-mobile');
+
+function toggleMenu(){
+    const nav= document.getElementById('nav');
+    nav.classList.toggle('active')
+}
+
+
+btnMobile.addEventListener('click', toggleMenu )
