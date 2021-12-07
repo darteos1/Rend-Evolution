54f34ebcd6f1cf7f68e4516877f9505fdaf552c9
Rafael Mota <rafael.m.cruz@ba.estudante.senai.br>
2021-12-07 16:20:19 -0300

    07/12 16:20
    atualizado

--- js/cadastrooo.js
+++ js/cadastrooo.js
@@ -1,20 +1,77 @@
-{/* <form action="#" method="POST">
-        <fieldset>
-            <legend>CADASTRO</legend>
-            <label for="nome">Nome:*</label><br>
-            <input type="text" placeholder="Nome" required id="nome"><br>
-            <label for="email">e-mail:</label><br>
-            <input type="email" placeholder="exemplo@exemplo.com" name="" id="email"><br>
-            <label for="cpf">CPF:</label><br>
-            <input type="tel" placeholder="000.000.000-00" required name="" id="cpf"><br>
-            <label for="senha">Senha:</label><br>
-            <input type="password" placeholder="*********" id="senha"><br>
-            <label for="tel">Telefone:</label><br>
-            <input type="tel" placeholder="(00) 01234-5678" id="telefone"><br>
-            <fieldset>
-                <legend>Sexo:</legend> 
-                <input type="radio" required name="sexo" value="M" id="sM">
-                <label for="sM">Masculino:</label>
-                <input type="radio" name="sexo" value="F" id="sF">
-                <label for="sF">Feminino:</label><br>
-            </fieldset> */}
+class Validator{
+    constructor(){
+        this.validations = [
+            'data-min-length',
+
+        ]
+    }
+// iniciar a validação de tudo
+validate(form){
+
+    // pegar os inputs
+    let inputs = form.getElementsByTagName('input');
+
+
+// transforma uma HTMLCollection -> array
+    let inputsArray = [...inputs];
+
+    //loop nos inputs e validação ao que for encontrado
+    inputsArray.forEach(function(input){
+        // todas as validações existentes
+        for(let i = 0; this.validations.length > i; i++){
+// verifica se a validação atual existe no input           
+            if(input.getAttribute(this.validations[i])!= null){
+                // limpando a string para virar um metodo
+                let method = this.validations[i].replace('data-', '').replace('-', '');
+
+                // valor do input
+                let value = input.getAttribute(this.validations[i]);
+
+                // invovar o metodo
+                this[method](input, value);
+                
+            }
+        }
+    }, this);   
+ }
+}
+// verifica se um input tem um numero minimo de caracteres
+ minlength(input, minValue) {
+    let inputLength = input.value.length;
+
+    let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
+    if(inputLength < minValue){
+        this.printMessage(input, errorMessage)
+    }
+};
+ // imprimir msg de erro na tela
+  printMessage(input, msg) {
+    let template = document.querySelector('.error-validation').cloneNode(true);
+
+    template.textContent = msg;
+    
+    let inputParent = input.parentNode;
+
+    template.classList.remove('template');
+
+    inputParent.appendChild(template);
+  }
+   
+
+
+
+
+
+
+let form = document.getElementById("register-form")
+let submit = document.getElementById("btn-submit")
+
+let validator = new Validator();
+
+// validações
+submit.addEventListener('click', function(e){
+   
+    e.preventDefault();
+    
+    validator.validate(form);
+});
