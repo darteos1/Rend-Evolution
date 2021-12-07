d29ed8ffa745dcaeb851072ee8ad9a11876e845f
Rafael Mota <rafael.m.cruz@ba.estudante.senai.br>
2021-12-06 23:36:10 -0300

    23:35 hrs 06/12
    AINDA N TA PRONTO

---
+++ js/cadastrooo.js
@@ -0,0 +1,20 @@
+{/* <form action="#" method="POST">
+        <fieldset>
+            <legend>CADASTRO</legend>
+            <label for="nome">Nome:*</label><br>
+            <input type="text" placeholder="Nome" required id="nome"><br>
+            <label for="email">e-mail:</label><br>
+            <input type="email" placeholder="exemplo@exemplo.com" name="" id="email"><br>
+            <label for="cpf">CPF:</label><br>
+            <input type="tel" placeholder="000.000.000-00" required name="" id="cpf"><br>
+            <label for="senha">Senha:</label><br>
+            <input type="password" placeholder="*********" id="senha"><br>
+            <label for="tel">Telefone:</label><br>
+            <input type="tel" placeholder="(00) 01234-5678" id="telefone"><br>
+            <fieldset>
+                <legend>Sexo:</legend> 
+                <input type="radio" required name="sexo" value="M" id="sM">
+                <label for="sM">Masculino:</label>
+                <input type="radio" name="sexo" value="F" id="sF">
+                <label for="sF">Feminino:</label><br>
+            </fieldset> */}
