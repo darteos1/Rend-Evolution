0c5a4fa65380137d0817853cd7d1fc6aa313cfa5
Guigas <guilhermeclreboucas@gmail.com>
2021-12-09 18:35:37 -0300

    Minha parte do Slide

--- js/main.js
+++ js/main.js
@@ -11,19 +11,42 @@ function carrossel(){
     }
     
     imgs.style.transform='translateX(${-idx*500}px)';
-
 }
 
 setInterval(carrossel, 1800);
 
-
-
 var email = document.getElementById("email")
 var senha = document.getElementById("password")
 
+// Slider
+function disableScroll() {
+    document.body.classList.add("stop-scrolling");
+}
 
+function enableScroll() {
+    document.body.classList.remove("stop-scrolling");
+}
 
+document.querySelector("#items").addEventListener('wheel', event => {
+    if(event.deltaY > 0) {
+        event.target.scrollBy(420, 0)
+    }else {
+        event.target.scrollBy(-420, 0)
+    }
+})
 
+// let count = 0
+// setInterval(function(){
+//   count += 400
+//   if(count > 8000) count = 0
+//   document.querySelector("#items").scrollTo(count, 0)
+//   console.log('moveu')
+// }, 1500)
 
-
-
+document.querySelector("#items").addEventListener("mouseenter", event => {
+    disableScroll()
+})
+document.querySelector("#items").addEventListener("mouseleave", event => {
+    enableScroll()
+})
+// Fim do Slider
