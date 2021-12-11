c35f1a07b87afd272bc50e457629634f987f1469
Rafael Mota <rafael.m.cruz@ba.estudante.senai.br>
2021-12-11 18:45:26 -0300

    18:45//// 11/12

--- js/cadastrooo.js
+++ js/cadastrooo.js
@@ -1,108 +1,3 @@
-// class Validator{
-//     // constructor(){
-//     //     this.validations = [
-//     //         'data-min-length',
-//     //         'data-max-length',
-
-//     //     ]
-//     // }
-// // iniciar a validação de tudo
-// validate(form){
-
-//     // resgata todas as validações
-//     let currentValidations = document.querySelectorAll('form .error-validation');
-
-//     if(currentValidations.length > 0) {
-//         this.cleanValidations(currentValidations);
-//     }
-
-//     // pegar os inputs
-//     let inputs = form.getElementsByTagName('input');
-
-
-// // transforma uma HTMLCollection -> array
-//     let inputsArray = [...inputs];
-
-//     //loop nos inputs e validação ao que for encontrado
-//     inputsArray.forEach(function(input){
-//         // todas as validações existentes
-//         for(let i = 0; this.validations.length > i; i++){
-// // verifica se a validação atual existe no input           
-//             if(input.getAttribute(this.validations[i])!= null){
-//                 // limpando a string para virar um metodo
-//                 let method = this.validations[i].replace('data-', '').replace('-', '');
-
-//                 // valor do input
-//                 let value = input.getAttribute(this.validations[i]);
-
-//                 // invovar o metodo
-//                 this[method](input, value);
-                
-//             }
-//         }
-//     }, this);   
-//  }
-
-// // verifica se um input tem um numero minimo de caracteres
-
-// //    minlength(input, minValue) {
-// //     let inputLength = input.value.length;
-
-// //     let errorMessage = `O campo precisa ter pelo menos ${minValue} caracteres`;
-// //     if(inputLength < minValue){
-// //         this.printMessage(input, errorMessage)
-// //     }
-// // }
-
-// // verifica se um input passou do limite de caracteres
-
-// // maxlength(input, maxValue){
-// //     let inputLength = input.value.length;
-
-// //     let errorMessage = `O campo precisa ter menos ${maxValue} caracteres`;
-// //     if(inputLength > maxValue){
-// //         this.printMessage(input, errorMessage)
-// //     }
-
-// // }
-//  // imprimir msg de erro na tela
-// //   printMessage(input, msg) {
-// //     let template = document.querySelector('.error-validation').cloneNode(true);
-
-// //     template.textContent = msg;
-    
-// //     let inputParent = input.parentNode;
-
-// //     template.classList.remove('template');
-
-// //     inputParent.appendChild(template);
-// //   }
-
-//   // limpa as validações
-//    cleanValidations(validations){
-//        validations.forEach(el => el.remove())
-//    }
-
-// }
-
-
-
-
-
-// let form = document.getElementById("register-form")
-// let submit = document.getElementById("btn-submit")
-
-// let validator = new Validator();
-
-// // validações
-// submit.addEventListener('click', function(e){
-   
-//     e.preventDefault();
-    
-//     validator.validate(form);
-// });
-
-
  
 function limpa_formulário_cep() {
     //Limpa valores do formulário de cep.
@@ -110,7 +5,6 @@ function limpa_formulário_cep() {
     document.getElementById('bairro').value=("");
     document.getElementById('cidade').value=("");
     document.getElementById('uf').value=("");
-    document.getElementById('ibge').value=("");
 }
 
 function meu_callback(conteudo) {
@@ -120,7 +14,6 @@ if (!("erro" in conteudo)) {
     document.getElementById('bairro').value=(conteudo.bairro);
     document.getElementById('cidade').value=(conteudo.localidade);
     document.getElementById('uf').value=(conteudo.uf);
-    document.getElementById('ibge').value=(conteudo.ibge);
 } //end if.
 else {
     //CEP não Encontrado.
@@ -148,7 +41,6 @@ if (cep != "") {
         document.getElementById('bairro').value="...";
         document.getElementById('cidade').value="...";
         document.getElementById('uf').value="...";
-        document.getElementById('ibge').value="...";
 
         //Cria um elemento javascript.
         var script = document.createElement('script');
