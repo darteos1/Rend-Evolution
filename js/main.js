e5a8fd13662fc8dd9c11c3a2e1351a02c439feb2
Daniel Alves <d.alves@ba.estudante.senai.br>
2021-12-14 23:40:15 -0300

    14:12 23:40d

--- js/main.js
+++ js/main.js
@@ -70,3 +70,24 @@ function dividir(){
 }
 
 new BeerSlider(document.getElementById('compare'))
+
+
+// =======================================================
+
+const btnMobile = document.getElementById('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
